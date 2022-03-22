589645b50da0f23dce2fae715d6118efc1c67d16
h_yashodha <h.yashodha@directfn.com>
2022-03-22 21:05:50 +0530

    Add settings file

--- src/app/utils/settings.ts
+++ src/app/utils/settings.ts
@@ -1,4 +1,7 @@
 export class Settings {
   // api
-  public static baseUrl = 'http://localhost:8000';
+  public static BASE_URL = 'http://localhost:8000';
+
+  // grid
+  public static DEFAULT_ROWS_PER_PAGE = 10;
 }
